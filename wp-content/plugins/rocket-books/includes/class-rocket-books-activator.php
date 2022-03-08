211a156f57b0ff07bdcbba648bc2a6d8ce0e07b1
Ahmad Raza <raza.ataki@gmail.com>
2022-03-08 15:29:15 +0500

    add empty plugin using wpbp.io

---
+++ wp-content/plugins/rocket-books/includes/class-rocket-books-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link        https://ahmadraza.ga
+ * @since      1.0.0
+ *
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/includes
+ * @author     Ahmad <raza.ataki@gmail.com>
+ */
+class Rocket_Books_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
