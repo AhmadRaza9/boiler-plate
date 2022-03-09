3db8d417742a654de359e4a404539ff619a5441f
Ahmad Raza <raza.ataki@gmail.com>
2022-03-09 11:34:56 +0500

    understanding tempalte hierarchy using chil theme

---
+++ wp-content/themes/twentysixteen-child/single-book.php
@@ -0,0 +1,9 @@
+<?php
+
+get_header();
+
+echo "Hello, i am from Child theme";
+
+get_footer()
+
+?>
---
+++ wp-content/themes/twentysixteen/single-book.php
@@ -0,0 +1,9 @@
+<?php
+
+get_header();
+
+echo "Hello, i am from parent theme";
+
+get_footer()
+
+?>
