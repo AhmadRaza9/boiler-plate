411d2e756d74cad2e3c9c35c1f416194a3ebb461
Ahmad Raza <raza.ataki@gmail.com>
2022-03-16 20:23:03 +0500

    settings page using form post method

--- wp-content/plugins/rocket-books/admin/class-rocket-books-admin.php
+++ wp-content/plugins/rocket-books/admin/class-rocket-books-admin.php
@@ -149,8 +149,8 @@ class Rocket_Books_Admin
 
     public function admin_page_display()
     {
-        // echo "I am page content";
-        include "partials/rocket-books-admin-display.php";
+        // Old method of saving options
+        include "partials/rocket-books-admin-display-form-method.php";
 
     }
 }
