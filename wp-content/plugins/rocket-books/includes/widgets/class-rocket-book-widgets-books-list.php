6af3152c5648159aa6db2c08f986c78a11f56384
Ahmad Raza <raza.ataki@gmail.com>
2022-03-21 17:57:08 +0500

    understanding widget API

--- wp-content/plugins/rocket-books/includes/widgets/class-rocket-book-widgets-books-list.php
+++ wp-content/plugins/rocket-books/includes/widgets/class-rocket-book-widgets-books-list.php
@@ -1,50 +1,66 @@
 <?php
-class Rocket_Books_Widget_Books_List extends WP_Widget
-{
 
-    /**
-     * Sets up the widgets name etc
-     */
-    public function __construct()
-    {
-        $widget_ops = array(
-            'classname' => 'my_widget',
-            'description' => 'My Widget is awesome',
-        );
-        parent::__construct('my_widget', 'My Widget', $widget_ops);
-    }
+/**
+ * Register Widget : Books List
+ *
+ * @package Rocket_Books
+ * @subpackage Rocket_Books/includes
+ * @author Ahmad <raza.ataki@gmail.com>
+ */
 
-    /**
-     * Outputs the content of the widget
-     *
-     * @param array $args
-     * @param array $instance
-     */
-    public function widget($args, $instance)
-    {
-        // outputs the content of the widget
-    }
+if (!class_exists('Rocket_Books_Widget_Books_List')) {
 
-    /**
-     * Outputs the options form on admin
-     *
-     * @param array $instance The widget options
-     */
-    public function form($instance)
+    class Rocket_Books_Widget_Books_List extends WP_Widget
     {
-        // outputs the options form on admin
-    }
 
-    /**
-     * Processing widget options on save
-     *
-     * @param array $new_instance The new options
-     * @param array $old_instance The previous options
-     *
-     * @return array
-     */
-    public function update($new_instance, $old_instance)
-    {
-        // processes widget options to be saved
+        /**
+         * Sets up the widgets name etc
+         */
+        public function __construct()
+        {
+            $widget_ops = array(
+                'classname' => 'rbr_books_list_class',
+                'description' => __("Display Roocket Books List", "rocket-books"),
+            );
+            parent::__construct('rbr_books_list', __("Books List ", "rocket-books"), $widget_ops);
+        }
+
+        /**
+         * Outputs the content of the widget
+         *
+         * @param array $args
+         * @param array $instance
+         */
+        public function widget($args, $instance)
+        {
+            // outputs the content of the widget
+            echo "This is widget method";
+        }
+
+        /**
+         * Outputs the options form on admin
+         *
+         * @param array $instance The widget options
+         */
+        public function form($instance)
+        {
+            // outputs the options form on admin
+            echo "This is form method";
+        }
+
+        /**
+         * Processing widget options on save
+         *
+         * @param array $new_instance The new options
+         * @param array $old_instance The previous options
+         *
+         * @return array
+         */
+        public function update($new_instance, $old_instance)
+        {
+            // processes widget options to be saved
+            $sanitized_instance = $new_instance;
+            return $sanitized_instance;
+        }
     }
 }
